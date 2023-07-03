eb90067db967afc46d3645d5348eea727e07c0be
ironwall <134495742+Ironwall72@users.noreply.github>
2023-07-03 00:09:55 -0300

    En este commit se agrega el primer models

--- pro1/apps/articulo/admin.py
+++ pro1/apps/articulo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Articulo,Categoria
 
 # Register your models here.
+
+@admin.register(Articulo)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id','titulo','resumen','contenido','fecha_publicacion','imagen','estado','categoria','publicado')
--- pro1/apps/articulo/models.py
+++ pro1/apps/articulo/models.py
@@ -1,3 +1,54 @@
+from typing import Any, Dict, Tuple
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=30, null=False)
+
+    def __str__(self):
+        return self.nombre
+
+class Articulo(models.Model):
+    #self.id = id
+    #self.id_usuario = id_usuario
+    titulo = models.CharField(max_length=30, null=False)
+    resumen = models.TextField(null=False)
+    contenido = models.TextField(null=False)
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+    imagen = models.ImageField(null=True, blank=True, upload_to='articulo',default='articulo/default_articulo.jpg')
+    estado = models.BooleanField(default=True)
+    categoria = models.ForeignKey(Categoria, on_delete = models.SET_NULL, null=True, default='Sin categoria')
+    publicado = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ('-publicado',)
+
+    def __str__(self):
+        return self.titulo
+        
+    def delete(self, using = None, keep_parents = False):
+        self.imagen.delete(self.imagen.name)
+        super().delete()
+        
+
+
+
+
+
+
+
+
+
+    #def publicar(self):
+        #if  self.online == True:
+         #   id = int(input("Ingrese el ID del articulo: "))
+          #  titulo = input("Ingrese el título del artículo: ")
+           # resumen = input("Ingrese el resumen del artículo: ")
+            #contenido = input("Ingrese el contenido del artículo: ")
+            #imagen = input("Ingrese la URL de la imagen del artículo (opcional): ")
+            #estado = "Activo"
+            #Articulo(id, self.id, titulo, resumen, contenido, datetime.now(), imagen, estado)
+        #else:
+         #   print("Para poder publicar debe loguearse.")
